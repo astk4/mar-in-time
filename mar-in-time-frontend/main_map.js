8fb4a25de42d1b450352070b746f75832e4eb140
Sofiia Burimova <sofiaburimova@gmail.com>
2025-12-27 00:41:23 +0200

    limited the map repetition and minimal zoom

--- mar-in-time-frontend/main_map.js
+++ mar-in-time-frontend/main_map.js
@@ -2,10 +2,27 @@
 import { portPopupTemplate } from "./popup_exports.js";
 
 var basicMap;
+let minLat = -85.05112878, maxLat = 85.05112878,
+    minLong = -180, maxLong = 180;
 
 function initMainMap() {
 
-    basicMap = L.map('mapid', { center: [20.0, 5.0], zoom: 4});
+    let myMaxBounds = [
+      [maxLat, maxLong], //south west
+      [minLat, minLong] //north east
+    ];
+    basicMap = L.map('mapid', 
+      { 
+        center: [20.0, 5.0], 
+        zoom: 4,
+        maxBounds: myMaxBounds, 
+        maxBoundsViscosity: 1.0
+      });
+    basicMap.setMinZoom(2);
+    basicMap.on('drag', function() {
+      basicMap.panInsideBounds(basicMap, { animate: false });
+    });
+
     L.tileLayer( 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
         subdomains: ['a','b','c'],
