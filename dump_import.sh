6e10dfdd1325ad464c76c9bd733289927821adff
Sofiia Burimova <sofiaburimova@gmail.com>
2026-07-23 01:40:48 +0300

    chore: hosted geojson data, added wget fetch if not mounted in container

--- dump_import.sh
+++ dump_import.sh
@@ -1,2 +1,10 @@
-pg_restore --no-owner --no-privileges -U some_pg_user -d marintime -v /usr/marintime_geodata.dump
+#!/bin/bash
+if [[ -e /usr/data_to_import/marintime_geodata.dump ]]
+then
+    echo "geojson data dump file already exists"
+else
+    wget -O /usr/data_to_import/marintime_geodata.dump "https://github.com/astk4/mar-in-time/releases/download/assets/marintime_geodata.dump"
+fi
+
+pg_restore --no-owner --no-privileges -U some_pg_user -d marintime -v /usr/data_to_import/marintime_geodata.dump
 psql -U some_pg_user -d marintime -c "ALTER TABLE eez_v12_copy RENAME TO eez_v12;"
