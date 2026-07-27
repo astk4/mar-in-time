1da66a07f089bc81ca2b12456d086cf63755cb2e
Sofiia Burimova <sofiaburimova@gmail.com>
2026-07-27 22:36:36 +0300

    chore: improved services/containers launch order

--- dump_import.sh
+++ dump_import.sh
@@ -8,3 +8,4 @@ fi
 
 pg_restore --no-owner --no-privileges -U some_pg_user -d marintime -v /usr/data_to_import/marintime_geodata.dump
 psql -U some_pg_user -d marintime -c "ALTER TABLE eez_v12_copy RENAME TO eez_v12;"
+psql -U some_pg_user -d marintime -c "CREATE TABLE IF NOT EXISTS docker_import_marker (completed_at TIMESTAMP DEFAULT NOW());"
